a9ad6475e12ae40897ff4e2e9958b84e8e9613e7
Hossein <hosseinkianmehr@yahoo.com>
2022-09-07 22:40:50 +0430

    create 404 page

---
+++ profile/pages/404.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Errorpage() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+        
+    <Image src="/svg/undraw_page_not_found_re_e9o6.svg" height='700hv' width='700wv' />
+    </div>
+  )
+}
