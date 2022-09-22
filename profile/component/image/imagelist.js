9ce915deef692f080aabfd88f809eb18e94891a3
Hossein <hosseinkianmehr@yahoo.com>
2022-09-22 14:48:42 +0330

    respansive image page => useeffect and usemeduaquery

--- profile/component/image/imagelist.js
+++ profile/component/image/imagelist.js
@@ -1,15 +1,30 @@
-import { Box, ImageList, ImageListItem } from '@mui/material'
+import { Box, ImageList, ImageListItem, useMediaQuery, useTheme } from '@mui/material'
 import Image from 'next/image';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Imagelist() {
+    const theme = useTheme()
+    const isTinyScreen = useMediaQuery(theme.breakpoints.down('md'))
+    const isTinyScreenSM = useMediaQuery(theme.breakpoints.down('sm'))
+    console.log(isTinyScreen,'isTinyScreen')
+    console.log(isTinyScreenSM,'isTinyScreenSM')
+
+    const [col, setcol] = useState(3)
+    useEffect(() => {
+        if (isTinyScreenSM == false & isTinyScreen == true) { setcol(2) }
+        else if (isTinyScreenSM == true & isTinyScreenSM == true) { setcol(1) }
+        else if (isTinyScreenSM == false & isTinyScreenSM == false) { setcol(3) }
+    }, [isTinyScreenSM,isTinyScreen])
+
+
+
     return (
         <div>
             <Box >
-                <ImageList variant="masonry" cols={3} gap={8}>
+                <ImageList variant="masonry" cols={col} gap={10}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
-                            <Image src={item.img} alt={item.title} width={item.cols == 2 ? 800:400} height={400} />
+                            <Image src={item.img} alt={item.title} width={item.cols == 2 ? 1200 : 800} height={800} />
                         </ImageListItem>
                     ))}
                 </ImageList>
